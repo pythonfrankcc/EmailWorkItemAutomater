f55d7d170ee2704fe569a2d0739dbef149f24fc5
pythonfrankcc <francismurithi475@gmail.com>
2023-01-30 04:52:35 +0300

    Completed assortment of inbox mails

--- InboxReader.py
+++ InboxReader.py
@@ -1,9 +1,13 @@
-# Importing libraries
+# Importing necessary libraries
+#libraries for email manipulation
 import imaplib, email
 
+#libraries for acquiring environment variables for storing password and username
 import os
+#libaries for calculation of elapsed time
+import time
 
-#the email address and password are set on machine environment variables
+#the email address and password are set on machine environment variables, you can use any of the below to get the user
 #user = os.getenv("EmailUser", default=None)
 user = os.environ.get('EmailUser')
 
@@ -22,18 +26,18 @@ def get_body(msg):
 
 # Function to search for a key value pair
 def search(key, value, con):
-    result, data = con.search(None, key, '"{}"'.format(value))
+    #result, data = con.search(None, key, '"{}"'.format(value))
+    result, data = con.search(None, key, value)
     return data
 
 # Function to get the list of emails under this label
 def get_emails(result_bytes):
-    msgs = [] # all the email data are pushed inside an array
+    msgs = [] # all the email data are pushed inside a list
     for num in result_bytes[0].split():
         typ, data = con.fetch(num, '(RFC822)')
         msgs.append(data)
  
-    return msgs[0:5]
-
+    return msgs
 #_______________________________________________________________________________________________________
 
 
@@ -46,6 +50,59 @@ con.login(user, password)
 # calling function to check for email under this label
 con.select('Inbox')
 
-# fetching emails from this user my email address
 msgs = get_emails(search('FROM', os.environ.get('EmailUser'), con))
-print(msgs)
+
+#finding the required content from the messages
+for msg in msgs[::-1]:
+    for sent in msg:
+        if type(sent) is tuple:
+ 
+            # encoding set as utf-8
+            content = str(sent[1], 'utf-8')
+            data = str(content)
+ 
+            # Handling errors related to unicodenecode
+            try:
+                indexstart = data.find("ltr")
+                data2 = data[indexstart + 5: len(data)]
+                indexend = data2.find("</div>")
+ 
+                # printing the required content which we need
+                # to extract from our email i.e our body
+                print(data2[0: indexend])
+ 
+            except UnicodeEncodeError as e:
+                pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#_________________________________________testing part to see whether the code works before hand_________________________
+#calculating the elapsed time it takes to call the function
+#record start time
+# start = time.perf_counter()
+
+# # fetching emails from this user my email address
+# msgs = get_emails(search('FROM', os.environ.get('EmailUser'), con))
+# print(msgs)
+# end = time.perf_counter()
+# time_in_seconds = end - start
+# print(time_in_seconds)
--- README.md
+++ README.md
@@ -12,9 +12,18 @@ PEP naming conventions as a standard
 https://www.python.org/dev/peps/pep-0008/#function-and-variable-names
 
 
-For this to work you will need to use an app password and it is important to note that on changing the password of your gmail account you will need to create a new app password
+For this to work you will need to use an app password and it is important to note that on changing the password of your gmail account you will need to create a new app password, ie , if you ever change your password.
+The password used in the script is an app password
 
 Link for the same:
 https://support.google.com/accounts/answer/185833#zippy=%2Capp-passwords-revoked-after-password-change
 
- 
+
+Here is a link on how to take advantage of your environment variables to store the username and password and avoid using them in code: https://www3.ntu.edu.sg/home/ehchua/programming/howto/Environment_Variables.html#:~:text=2.2%20Set%2FUnset%2FChange%20an,it%20to%20an%20empty%20string.
+
+For it to take effect on test restart the interpreter and list the variables using the below code:
+for key in os.environ:
+    print(key, '=>', os.environ[key])
+
+
+On using the per_counter on the script, I found that the rough execution time is 8.44 minutes which is roughly 9 minutes
