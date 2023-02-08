3124f1d86496705d452a964469a68dba02fbd1ff
pythonfrankcc <francismurithi475@gmail.com>
2023-02-08 04:57:59 +0300

    fixing the update issue on sqlite and chenging to the new app version

--- InboxReader1_version1_test.py
+++ InboxReader1_version1_test.py
@@ -19,18 +19,30 @@ imap.login(user, password)
 # select the e-mails
 res, messages = imap.select('Inbox')
 
+
+#functionof the user's email that you want to use
+def search(key, value, con):
+    result, data = con.search(None, key, '"{}"'.format(value))
+    #result, data = con.search(None, key, value)
+    print("The type of data that is returned by con.search is:", type(data))
+    print("Here is a sample of the data list returned: ", data[0])
+    return data
+
+msgs = search('FROM', os.environ.get('EmailUser'), messages)
 # calculates the total number of Incoming messages
-messages = int(messages[0])
+inbox_messages = int(msgs[0])
 
 # determine the number of e-mails to be fetched
 n = 3
 
 # iterating over the e-mails
-for i in range(messages, messages - n, -1):
+for i in range(inbox_messages, inbox_messages - n, -1):
     res, msg = imap.fetch(str(i), "(RFC822)")   
     for response in msg:
         if isinstance(response, tuple):
             msg = email.message_from_bytes(response[1])
+            #print the whole message to see what you get
+            print(msg)
             
             # getting the sender's mail id
             From = msg["From"]
@@ -38,6 +50,17 @@ for i in range(messages, messages - n, -1):
             # getting the subject of the sent mail
             subject = msg["Subject"]
 
+            #placing the data in a file for read by the sticky notes, this is in append mode
+            file1 = open(r"StickyNoteFile.txt", "a")
+            file1.write(subject)
+
             # printing the details
             print("From : ", From)
             print("subject : ", subject)
+
+#you should close the connection and also logout
+imap.close()
+imap.logout()
+
+#close file1 to free the memory acquired by file1
+file1.close()
--- Sticky Notes Code/notes.py
+++ Sticky Notes Code/notes.py
@@ -1,4 +1,5 @@
 #Import Necessary modules
+import time
 import sqlite3 as sql
 from tkinter import *
 from tkinter import messagebox
@@ -17,14 +18,18 @@ except:
 # Insert a row of data
 def add_notes():
         #Get input values
-        today = date_entry.get()
+        #today = date_entry.get()
+        today = str(time.ctime())
         notes_title = notes_title_entry.get()
         notes = notes_entry.get("1.0", "end-1c")
         #Raise a prompt for missing values
+        # if (len(today) <=0) & (len(notes_title)<=0) & (len(notes)<=1):
+        #         messagebox.showerror(message = "ENTER REQUIRED DETAILS" )
         if (len(today) <=0) & (len(notes_title)<=0) & (len(notes)<=1):
                 messagebox.showerror(message = "ENTER REQUIRED DETAILS" )
         else:
         #Insert into the table
+                # cur.execute("INSERT INTO notes_table VALUES ('%s','%s','%s')" %(today, notes_title, notes))
                 cur.execute("INSERT INTO notes_table VALUES ('%s','%s','%s')" %(today, notes_title, notes))
                 messagebox.showinfo(message="Note added")
         #Commit to preserve the changes
@@ -87,6 +92,7 @@ def delete_notes():
 #Update the notes
 def update_notes():
         #Obtain user input
+        #today = str(time.ctime())
         today = date_entry.get()
         notes_title = notes_title_entry.get()
         notes = notes_entry.get("1.0", "end-1c")
